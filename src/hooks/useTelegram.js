6ea3c3e798bd264018743c7fd45e4891f2ce29fd
anna <50797880+matela-anya@users.noreply.github.com>
2025-01-02 00:37:53 +0300

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -3,15 +3,29 @@ import { useEffect } from 'react';
 export const useTelegram = () => {
   const tg = window.Telegram.WebApp;
 
-  useEffect(() => {
+  const init = () => {
     tg.ready();
+  };
+
+  useEffect(() => {
+    init();
   }, []);
 
   const close = () => tg.close();
+  const showMainButton = (text) => {
+    tg.MainButton.setText(text);
+    tg.MainButton.show();
+  };
+
+  const hideMainButton = () => {
+    tg.MainButton.hide();
+  };
 
   return {
     tg,
+    init,
     close,
+    showMainButton,
+    hideMainButton,
   };
 };
-
