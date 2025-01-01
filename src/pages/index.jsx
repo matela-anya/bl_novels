c84d8bc837ba91a5985440c9efb9eda72066a014
anna <50797880+matela-anya@users.noreply.github.com>
2025-01-02 00:31:56 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import NovelList from '../components/NovelList';
+import Layout from '../components/Layout';
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <h1>Список новелл</h1>
+      <NovelList />
+    </Layout>
+  );
+};
+
+export default HomePage;
 
