8f82ac00e71211228c3c1bb81abc83272f47f4dd
anna <50797880+matela-anya@users.noreply.github.com>
2025-01-02 00:30:46 +0300

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,1 +1,17 @@
+import { useEffect } from 'react';
+
+export const useTelegram = () => {
+  const tg = window.Telegram.WebApp;
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const close = () => tg.close();
+
+  return {
+    tg,
+    close,
+  };
+};
 
